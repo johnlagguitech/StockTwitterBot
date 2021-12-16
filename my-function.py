1e52ffa7a7885b79b318657a69c557c1a6983e29
johnlagguitech <johnlaggui@gmail.com>
2021-12-16 07:44:19 -0600

    Update my-function.py

--- my-function.py
+++ my-function.py
@@ -3,6 +3,9 @@ import json
 def lambda_handler(event, context):
     # TODO implement
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from MY COMPUTER!')
+        "statusCode": 200,
+        "body": json.dumps({
+            "abc": ip.status_code,
+            "message": os.environ.get('MyParam')
+        }),
     }
